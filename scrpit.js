490d98f768a6ef244a169f58becb08d4e09bc258
kumarana007 <124980096+kumarana007@users.noreply.github.com>
2025-02-08 17:48:28 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,49 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+
+
+
+function AddTask() {
+    if (inputBox.value === '') {
+
+        alert("You Must Write Something ! ! ")
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    inputBox.value = ""
+    savedata();
+}
+
+
+
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        savedata()
+    } else if (e.target.tagName === "SPAN") {
+
+        e.target.parentElement.remove();
+        savedata()
+
+    }
+
+}, false)
+
+
+function savedata() {
+    localStorage.setItem("data", listcontainer.innerHTML);
+
+}
+
+function showTask() {
+    listcontainer.innerHTML = localStorage.getItem("data");
+
+}
+showTask()
+    // const good= document.getElementsByClassName("checked")
